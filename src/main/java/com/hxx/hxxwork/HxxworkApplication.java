d49cbcacfb988a05b821391c1907c4fd31727efd
kadintt <1959665514@qq.com>
2023-03-26 17:28:26 +0800

    Initial commit

---
+++ src/main/java/com/hxx/hxxwork/HxxworkApplication.java
@@ -0,0 +1,13 @@
+package com.hxx.hxxwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HxxworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HxxworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hxx/hxxwork/HxxworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hxx.hxxwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HxxworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
